c1244d3ed81c4da65d66702c3eaf5bedc07d321c
PD <89239979+PDgaming@users.noreply.github.com>
2023-12-22 15:45:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ async function animateText(text) {
         if (i === text.length) {
             clearInterval(interval);
         }
-    }, 90);
+    }, 100);
 }
 
 async function scene1() {
@@ -52,9 +52,9 @@ async function scene2() {
     await sleep(1000);
     rainbowTitle.stop();
     await animateText(`Jingle stumbles upon Rani, a resourceful village child who immediately offers to help. Together, they follow a trail of twinkling lights from the wreckage, leading them to an ancient mango tree known for its magical aura. Rani explains that the tree spirits might hold the key to finding the lost list.
-Inside the tree's hollow trunk, they discover a mischievous Monkey Spirit sporting a Santa hat. He reveals that ${chalk.red("the list has scattered into magical gingerbread cookies")}, each representing a different region of India and holding fragments of the Naughty and Nice information. ${chalk.green("To retrieve them, Jingle and Rani must embark on a festive quest across the country.")}`);
-    await sleep(64500);
-}
+Inside the tree's hollow trunk, they discover a mischievous Monkey Spirit sporting a Santa hat. He reveals that ${chalk.red("the list has scattered into magical gingerbread cookies")}, each representing a different region of India and holding fragments of the Naughty and Nice information. To retrieve them, Jingle and Rani must embark on a festive quest across the country.\n`);
+    await sleep(74000);
+};
 
 // console.log("They face a hidden challenge - a grumpy elf named Grumpo, who believes Christmas should only be celebrated in traditional ways and tries to sabotage their journey.")
 
@@ -63,21 +63,27 @@ async function scene3() {
     await sleep(1000);
     rainbowTitle.stop();
     await animateText(`Rani, who was initially skeptical about Christmas, begins to appreciate its spirit of giving and inclusivity, while Jingle learns to understand and respect the diverse Christmas traditions of India.
- ${chalk.red("Jingle and Rani must use their coding skills and newfound understanding of Indian traditions to win him over and teach him the true meaning of Christmas.")}`)
-    await sleep(45000);
-}
+ ${chalk.red("Jingle and Rani must use their coding skills and newfound understanding of Indian traditions to win him over and teach him the true meaning of Christmas.")}\n`)
+    await sleep(40500);
+};
 
 async function scene4() {
     const rainbowTitle = chalkAnimation.rainbow("Scene 4: A Festive Finale:")
-    await sleep();
+    await sleep(1000);
     rainbowTitle.stop();
-    await animateText(`With all the gingerbread cookies recovered, Jingle and Rani return to the mango tree. The Monkey Spirit uses them to reassemble the Naughty and Nice List, revealing a heartwarming surprise: every child in India, naughty or nice, deserves a Christmas miracle.
-Jingle, filled with gratitude and newfound knowledge, delivers the gifts with Rani by his side, celebrating Christmas under the mango tree with the entire village. They learn that the magic of Christmas isn't about list-making or tradition, but about the joy of giving, the warmth of community, and the love that transcends cultural differences.`)
-    await sleep(63000);
-}
+    await animateText(`${chalk.yellow(`With all the gingerbread cookies recovered, Jingle and Rani return to the mango tree. The Monkey Spirit uses them to reassemble the Naughty and Nice List, revealing a heartwarming surprise: every child in India, naughty or nice, deserves a Christmas miracle.
+Jingle, filled with gratitude and newfound knowledge, delivers the gifts with Rani by his side, celebrating Christmas under the mango tree with the entire village. They learn that the magic of Christmas isn't about list-making or tradition, but about the joy of giving, the warmth of community, and the love that transcends cultural differences.`)}\n`)
+    await sleep(67500);
+};
+
+async function ending() {
+    const rainbowTitle = chalkAnimation.rainbow("Thank you so much for playing this game!");
+    await sleep(3000);
+    rainbowTitle.stop();
+};
 
 // await scene1();
-await scene2();
-await scene3();
-// await scene4();
-console.log("Thank you so much for playing this game!");
+// await scene2();
+// await scene3();
+await scene4();
+await ending();
